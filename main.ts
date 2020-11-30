c622f3070014752174fff3d3bb50cbab324a8fb5
Alan B. Christie <alan@kaitak.co.uk>
2020-11-30 09:40:22 +0000

    Adds comment block

--- main.ts
+++ main.ts
@@ -1,3 +1,8 @@
+/**
+ * Code used in conjunction with "btn_notify.py" code running on a Raspberry Pi to verify Bluetooth connectivity. A basic code block simply to act as a connectivity test.
+ * 
+ * See https://github.com/ukBaz/python-bluezero/issues/279 for a background to the problem and its on-going testing.
+ */
 bluetooth.onBluetoothConnected(function () {
     basic.showString("C")
     serial.writeLine("C")
